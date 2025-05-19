af685f0b58fc3577fd936fd754ffcfdbcd3689d0
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-05-19 07:44:20 +0530

    Register Employee and Attendance models in the Django admin interface

--- face_app/admin.py
+++ face_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Employee, Attendance
 
-# Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'employee_id', 'created_at']
+    search_fields = ['name', 'employee_id']
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ['employee', 'date', 'check_in_time']
+    list_filter = ['date']
